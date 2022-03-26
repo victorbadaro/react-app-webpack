ffe7d6c189bd8068617b5722060b373fc5c71ac1
Victor <victor.badaro@fatec.sp.gov.br>
2022-03-26 20:54:24 -0300

    feat: add webpack-dev-server lib and its configs

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,7 +1,16 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
+const path = require('path');
 
 module.exports = merge(common, {
     mode: 'development',
-    devtool: 'eval-source-map'
+    devtool: 'eval-source-map',
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, '..', 'public')
+        },
+        hot: true,
+        port: 3000,
+        open: true
+    }
 });
