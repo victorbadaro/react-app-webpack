874a6558f7bb57a6333cf8603589f8a12bb40dd9
Victor <victor.badaro@fatec.sp.gov.br>
2022-03-26 20:49:34 -0300

    chore: set the html-webpack-plugin

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,14 +1,22 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src', 'index.jsx'),
+    entry: {
+        bundle: path.resolve(__dirname, '..', 'src', 'index.jsx')
+    },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        path: path.resolve(__dirname, '..', 'dist'),
+        filename: '[name].[contenthash].js'
     },
     resolve: {
         extensions: ['.js', '.jsx']
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, '..', 'public', 'index.html')
+        })
+    ],
     module: {
         rules: [
             {
