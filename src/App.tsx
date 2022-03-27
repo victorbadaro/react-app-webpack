961ce0c21769c8fbe303d8ab7536df98ffb78b52
Victor <victor.badaro@fatec.sp.gov.br>
2022-03-26 21:16:10 -0300

    chore: change App component content

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { useState } from 'react';
+
 export function App() {
+    const [counter, setCounter] = useState(0);
+
+    function handleAddButtonClick() {
+        setCounter(counter + 1);
+    }
+
     return (
-        <h1>Hello World!</h1>
+        <>
+            <h1>You have clicked {counter} on the button below!</h1>
+            <button
+                type="button"
+                onClick={handleAddButtonClick}
+            >
+                Add 1
+            </button>
+        </>
     );
 }
